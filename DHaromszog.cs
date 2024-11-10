6173aa5e97c315fb25f2ce1f521fbff4906ef230
meszarostibor <meszarostibor74@outlook.hu>
2024-11-10 19:56:56 +0100

    Initial commit

---
+++ DHaromszog.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DerekszoguHaromszogek
+{
+    public class DHaromszog
+    {
+        private double aOldal;
+        private double bOldal;
+        private double cOldal;
+        public double a
+        {
+            get { return aOldal; }
+            set
+            {
+                if (value > 0)
+                {
+                    aOldal = value;
+                }
+                else
+                {
+                    throw new Exception("Az 'a' oldal nem lehet 0, vagy negatív!");
+                }
+            }
+        }
+        public double b
+        {
+            get { return bOldal; }
+            set
+            {
+                if (value > 0)
+                {
+                    bOldal = value;
+                }
+                else
+                {
+                    throw new Exception("A 'b' oldal nem lehet 0, vagy negatív!");
+                }
+            }
+        }
+        public double c
+        {
+            get { return cOldal; }
+            set
+            {
+                if (value > 0)
+                {
+                    cOldal = value;
+                }
+                else
+                {
+                    throw new Exception("A 'c' oldal nem lehet 0, vagy negatív!");
+                }
+            }
+        }
+
+        private bool EllDerekszogu {
+            get
+            {
+                if ((a * a) + (b * b) == (c * c))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }    
+            }
+        }
+        private bool EllMegszerkesztheto 
+        {
+            get 
+            {
+                if (a + b > c) 
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        private bool EllNovekvoSorrend 
+        {
+            get 
+            {
+                if ((b > a) & (c > b))
+                {
+                    return true;
+                }
+                else 
+                {
+                    return false;
+                }
+            }
+        }
+
+
+        public double Kerulet 
+        {
+            get
+            {
+                return a + b + c;
+            }
+        }
+        public double Terulet 
+        {
+            get
+            {                
+                return Math.Round(((a * b) / 2), 2);
+            }
+        }
+        public int sorSzama 
+        { 
+            get; 
+            set; 
+        }
+
+        public DHaromszog(string sor, int sorSzama)
+        {
+            string[] oldalak = sor.Split(' ');
+            this.a = double.Parse(oldalak[0]);
+            this.b = double.Parse(oldalak[1]);
+            this.c = double.Parse(oldalak[2]);
+            this.sorSzama = sorSzama;
+
+            if (EllNovekvoSorrend == false)
+            {
+                throw new Exception("Az adatok nincsenek növekvő rendben!");
+            }
+
+            if (EllMegszerkesztheto == false) 
+            {
+                throw new Exception("A háromszöget nem lehet megszerkeszteni!");
+            }
+
+            if (EllDerekszogu == false)
+            {
+                throw new Exception("A háromszög nem derékszögű!");
+            }
+        }
+
+
+        public override string ToString() {
+            return $"{sorSzama} sor: a={a} b={b} c={c}";
+        }
+
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DerekszoguHaromszogek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<DHaromszog> dHaromszogek = new List<DHaromszog>();
+
+        private void OpenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            //dialog.InitialDirectory = "";
+            dialog.FileName = ""; 
+            dialog.DefaultExt = ".txt";
+            dialog.Filter = "Text documents (.txt)|*.txt";
+
+            bool? result = dialog.ShowDialog();
+           
+            if (result == true)
+            {
+                dHaromszogek.Clear();   
+                hibakLista.Items.Clear();
+                derekszoguLista.Items.Clear();
+                kivalasztottLista.Items.Clear() ;    
+
+                string filename = dialog.FileName;
+
+                StreamReader reader = new StreamReader(filename);
+
+                int sorszam = 0;
+
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    
+
+                    try
+                    {
+                        sorszam++;
+                        dHaromszogek.Add(new DHaromszog(line, sorszam));    
+                    }
+                    catch (Exception ex) 
+                    {
+                        hibakLista.Items.Add($"{sorszam}.sor: {ex.Message}");
+                    }
+                }
+
+                foreach (DHaromszog d in dHaromszogek) 
+                { 
+                    derekszoguLista.Items.Add(d);
+                }    
+
+                reader.Close();
+            }
+
+        }
+
+        private void derekszoguLista_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            kivalasztottLista.Items.Clear();
+            kivalasztottLista.Items.Add($"Kerület = {dHaromszogek[derekszoguLista.SelectedIndex].Kerulet}");
+            kivalasztottLista.Items.Add($"Terület = {dHaromszogek[derekszoguLista.SelectedIndex].Terulet}");
+        }
+    }
+}
